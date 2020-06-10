82c74c9596f86aae75fe6fcb85f454a68c9ff76b
C. Vilsmeier <vilsmeier@gmx.de>
2020-06-10 16:51:17 +0200

    travis one more

--- .travis.install-sqinn.sh
+++ .travis.install-sqinn.sh
@@ -3,7 +3,7 @@
 echo HOME is $HOME
 echo we are in `pwd`
 echo installing sqinn
-curl -L https://github.com/cvilsmeier/sqinn/releases/download/1.0.0/sqinn-dist-1.0.0.tar.gz -o /tmp/sqinn-dist-1.0.0.tar.gz
+curl -L https://github.com/cvilsmeier/sqinn/releases/download/v1.0.0/sqinn-dist-1.0.0.tar.gz -o /tmp/sqinn-dist-1.0.0.tar.gz
 tar -C /tmp -xf /tmp/sqinn-dist-1.0.0.tar.gz
 chmod a+x /tmp/sqinn-dist-1.0.0/linux_amd64/sqinn
 echo ls -al
